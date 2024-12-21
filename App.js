becb15aceb33478d4c4b96c49a15ab8dc92ea350
NotNaddie <nadie0971@gmail.com>
2024-12-21 10:47:33 -0600

    Commit

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style = {styles.container}>
+      <Text style = {styles.text}>Waa!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#ffffff',
+    fontWeight: 'bold'
+  }
 });
