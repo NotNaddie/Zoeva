9ff31d8cbe913ac3321f8152025aa6a526f39951
NotNaddie <nadie0971@gmail.com>
2025-01-07 22:44:15 -0600

    SamuIS

--- App.js
+++ App.js
@@ -1,29 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SvgComponent from './SVGComponent';
-
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity} from 'react-native';
+import Boton from './Boton';
 export default function App() {
   return (
-    <View style = {styles.container}>
-      <Text style = {styles.text}>Hola Mundo</Text>
-      <SvgComponent viewBox = "0 0 800 800" width="200" height="200"/>
-    </View>
+    <View style={styles.mainContainer}>
+      <StatusBar style="auto" />
+      <View>
+        <Image source={require('./assets/ZoevaLogoL.png')} style={styles.LogoZoeva} />
+      </View>
+
+      <View style={styles.containerPrincipalIS}>
+        <View style={styles.containerSecundarioIS}>
+          <TextInput style={styles.TextInputs} placeholder='/User' />
+          <TextInput style={styles.TextInputs} placeholder='/Password' />
+          <Text style={styles.TituloContainerPrincipal}>Log in</Text>
+           <Boton/>
+        </View>
+
+        </View>
+      </View>
+      
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '#0F705F',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#ffffff',
-    fontWeight: 'bold'
+  // IS = Iniciar Sesión
+  containerSecundarioIS: {
+    backgroundColor: '#EDF3D4',
+    width: 310,
+    height: 400,
+    borderRadius: 20,
+    marginBottom: 20,
+    alignContent: 'center',
+    justifyContent: 'center',
+    transform: [{translateX: -8}, {translateY: -15}],
   },
-  svg:{
-    width: "100%",
-    height: "100%"
-  }
+  containerPrincipalIS: {
+    backgroundColor: '#3F8674',
+    width: 320,
+    height: 400,
+    borderRadius: 20,
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset : {width: 10, height: 10},
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 30,
+    transform: [{translateX: 10}, {translateY: -65}],
+  },
+  TextInputs: {
+    fontSize: 14,
+    height: 32,
+    borderRadius: 40,
+    width: 250,
+    borderWidth: -5,
+    borderColor: '#DADADA',
+    backgroundColor:'#DADADA',
+    borderColor: '#DADADA',
+    transform: [{translateX: 35}, {translateY: -10}],
+    padding: 5,
+    paddingStart: 10,
+    marginTop: 40,
+    elevation: 20,
+    font: 'Roboto'
+  },
+  LogoZoeva: {
+    width: '350',
+    height: 250,
+    borderRadius: 100,
+    transform: [{translateX: -13}, {translateY: -80}],
+  } ,
+  TituloContainerPrincipal: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#33566C',
+    transform: [{translateX: 120}, {translateY: -190}],
+  },
 });
---
+++ Boton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function Boton() {
+    return (
+        <TouchableOpacity>
+            <LinearGradient
+                colors={['#548196', '#6CAACF']}
+                style={styles.Boton}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+            >
+                <Text style={styles.TextoBoton}>Sign in</Text>
+            </LinearGradient>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    Boton: {
+        width: 230,
+        height: 50,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        transform: [{translateX: 45}, {translateY: 10}],
+    },
+    TextoBoton: {
+        color: 'white',
+        fontSize: 26,
+        fontWeight: 'bold',
+    }
+  });
