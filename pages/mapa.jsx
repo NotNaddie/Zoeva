9d600632411d21c408dd929adbdc37d47fa0fd85
NotNaddie <nadie0971@gmail.com>
2025-01-13 21:52:18 -0600

    Refactorizacion inicio

---
+++ pages/mapa.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, SafeAreaView, TouchableOpacity } from 'react-native';
+
+const Mapa = () => {
+    <View>
+        
+    </View>
+}
+
+export default Mapa
